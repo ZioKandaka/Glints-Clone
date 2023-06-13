f97f99e477cda34d04da5e3a343e2a448172d1c5
ZioKandaka <ziokaelani@gmail.com>
2023-06-14 03:42:19 +0700

    renew

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
